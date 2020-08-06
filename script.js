6511ee4b7d15c0356031702c6e5e658a9d340d00
John Mark Goodson <john.mark.hat@gmail.com>
2020-08-06 14:58:33 -0400

    added color references to script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+//
+
+// 000000 - black
+// 14213D - dark blue
+// FCA311 - light orange
+// E5E5E5 - light grey
+// FFFFFF - white
+
+// rgb(0, 0, 0) - black
+// rgb(20,33,61) - dark blue
+// rgb(252,163,17)- light orange
+// rgb(229, 229, 229) - light grey
+// rgb(255, 255, 255) - white
